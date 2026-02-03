984ecf4abe6e0f11b12741e6c344ee4f60d32252
risha-ashwin <rishamashwin@gmail.com>
2026-02-03 01:46:11 -0800

    Center Time Display + Base

--- sketches/sketch3.js
+++ sketches/sketch3.js
@@ -1,14 +1,33 @@
 // Instance-mode sketch for tab 3
+// HWK 4, Sketch B - Progress Ring Clock
+
 registerSketch('sk3', function (p) {
   p.setup = function () {
-    p.createCanvas(p.windowWidth, p.windowHeight);
-  };
-  p.draw = function () {
-    p.background(240, 200, 200);
-    p.fill(180, 60, 60);
-    p.textSize(32);
+    p.createCanvas(600, 600);
+    p.angleMode(p.DEGREES);
     p.textAlign(p.CENTER, p.CENTER);
-    p.text('HWK #4. B', p.width / 2, p.height / 2);
   };
-  p.windowResized = function () { p.resizeCanvas(p.windowWidth, p.windowHeight); };
+
+  p.draw = function () {
+    p.background(248);
+
+    let hour = p.hour()
+    let minute = p.minute();
+    let second = p.second();
+
+    let minuteText = minute;
+    if (minute < 10) {
+      minuteText = "0" + minute;
+    }
+
+    let secondText = second;
+    if (second < 10) {
+      secondText = "0" + second;
+    }
+
+    p.fill(20);
+    p.noStroke();
+    p.textSize(32);
+    p.text(hour + ":" + minuteText + ":" + secondText, p.width / 2, p.height / 2);
+  };
 });

75e5af646f33e9601affeba68b015e9e8fe972ea
risha-ashwin <rishamashwin@gmail.com>
2026-02-04 20:19:28 -0800

    Basic Candle Shape

--- sketches/sketch4.js
+++ sketches/sketch4.js
@@ -1,14 +1,20 @@
 // Instance-mode sketch for tab 4
+// HWK 4, Sketch C - Candle Clock
 registerSketch('sk4', function (p) {
   p.setup = function () {
-    p.createCanvas(p.windowWidth, p.windowHeight);
+    p.createCanvas(400, 400);
+    p.noLoop();
   };
   p.draw = function () {
-    p.background(200, 240, 200);
-    p.fill(30, 120, 40);
-    p.textSize(32);
-    p.textAlign(p.CENTER, p.CENTER);
-    p.text('HWK #4. C', p.width / 2, p.height / 2);
+    p.background(245);
+    p.translate(p.width / 2, p.height / 2);
+
+    p.fill(240);
+    p.noStroke();
+    p.rect(-40, -200, 80, 300, 10);
+
+    p.fill(255, 180, 80);
+    p.ellipse(0, -220, 30, 45);
   };
-  p.windowResized = function () { p.resizeCanvas(p.windowWidth, p.windowHeight); };
+  
 });

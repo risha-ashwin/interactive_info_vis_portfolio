40fc108be4daf61df5300d93657a10d3d2bdee26
risha-ashwin <rishamashwin@gmail.com>
2026-02-06 17:31:13 -0800

    Clock

--- sketches/sketch2.js
+++ sketches/sketch2.js
@@ -2,9 +2,8 @@
 // HWK 4, Sketch A - Dancer Poses Clock
 
 registerSketch('sk2', function (p) {
-  registerSketch('sk2', function (p) {
   p.setup = function () {
-    p.createCanvas(700, 700);
+    p.createCanvas(700, 700); 
     p.angleMode(p.DEGREES);
     p.textAlign(p.CENTER, p.CENTER);
   };
@@ -15,6 +14,28 @@ registerSketch('sk2', function (p) {
     let hour = p.hour();
     let minute = p.minute();
 
+    let hour12 = hour % 12;
+    if (hour12 === 0) {
+      hour12 = 12;
+    }
+
+    let minuteText = minute < 10 ? "0" + minute : "" + minute;
+
+    p.translate(p.width / 2, p.height / 2);
+
+    p.noFill();
+    p.stroke(220);
+    p.strokeWeight(10);
+    p.circle(0, 0, 560);
+
+    p.noStroke();
+    p.fill(60);
+    p.circle(0, 0, 10);
+
+    p.fill(40);
+    p.textSize(32);
+    p.text("Time: " + hour12 + ":" + minuteText, 0, 0);
   };
-});
+
+  p.windowResized = function () { };
 });
